97785ff5e7675c9d466d74dc6818310360c5084d
hhgg0017@gmail.com <hhgg0017@gmail.com>
2023-01-29 20:25:40 +0900

    sql 연습

--- db/sql_쿼리연습.sql
+++ db/sql_쿼리연습.sql
@@ -9,4 +9,13 @@ select
 from 
 	user_mst as um
 left outer join role_dtl rd on (rd.user_id = um.user_id)
-left outer join role_mst rm on (rm.role_id = rd.role_id)
+left outer join role_mst rm on (rm.role_id = rd.role_id);
+
+select
+	*
+from
+	user_mst um
+    left outer join role_dtl rd on (rd.user_id = um.user_id)
+    left outer join role_mst rm on (rm.role_id = rd.role_id)
+where
+	um.username = "aaa";
