ae61c43f28c4c2a99e41c0f97d770d4f6aa7cd86
JungSungHyun <hhgg0017@gmail.com>
2023-02-10 13:38:32 +0900

    2023. 02.10 socket

--- src/simplechatting/server/ServerApplication.java
+++ src/simplechatting/server/ServerApplication.java
@@ -8,9 +8,76 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+
+@Data
+class ConnectedSocket extends Thread{
+	
+	private static List<ConnectedSocket> socketList = new ArrayList<>();
+	private Socket socket;
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	
+	private String username;
+	
+	public ConnectedSocket(Socket socket) {
+		this.socket = socket;
+		socketList.add(this);
+	}
+	
+	@Override
+	public void run() {
+		try {
+			outputStream = socket.getOutputStream();
+			PrintWriter out = new PrintWriter(outputStream, true);
+			out.println("join");
+
+			inputStream = socket.getInputStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
+			username = in.readLine();
+
+			System.out.println(username + "님이 접속하였습니다.");
+	
+			String userListStr = "";
+			
+			for (int i = 0; i<socketList.size(); i++) {
+				userListStr += socketList.get(i).getUsername();
+				if(i < socketList.size() - 1) {
+					userListStr +=",";
+				}
+			}
+			
+			for (ConnectedSocket connectedSocket : socketList) {
+				outputStream = connectedSocket.getSocket().getOutputStream();
+				out = new PrintWriter(outputStream, true);
+				out.println("@welcome/" + username + "님이 접속하였습니다.");
+				out.println("@userList/" + userListStr);
+			}
+
+			while (true) {
+				String message = in.readLine();
+				for (ConnectedSocket connectedSocket : socketList) {
+					outputStream = connectedSocket.getSocket().getOutputStream();
+					out = new PrintWriter(outputStream, true);
+					out.println(message);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
 
 public class ServerApplication {
-	
+
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
 		try {
@@ -18,35 +85,24 @@ public class ServerApplication {
 			System.out.println("=======<<server start>>=======");
 			
 			
-			Socket socket =	serverSocket.accept(); // client의 접속을 기다리는 method
-			System.out.println(socket.getInetAddress() + "success connect client");
 			
-			OutputStream outputStream = socket.getOutputStream();
-			PrintWriter out = new PrintWriter(outputStream,true);
-			out.println("join");
-			
-			InputStream inputStream = socket.getInputStream();
-			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-			
-			String welcomeMessage = in.readLine();
-			System.out.println(welcomeMessage);
-			out.println(welcomeMessage);
-			
-			while(true) {
-				in.readLine();
+			while (true) {
+				Socket socket = serverSocket.accept(); // client의 접속을 기다리는 method
+				ConnectedSocket connectedSocket = new ConnectedSocket(socket);
+				connectedSocket.start();
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			if(serverSocket != null) {
+			if (serverSocket != null) {
 				try {
 					serverSocket.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			
+
 			System.out.println("=======<<server end!!>>========");
 		}
 	}
